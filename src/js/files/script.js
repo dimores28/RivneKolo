df287035d93dbaa9b11a97370668e6aa1980636a
Dmytro <dmytroivanovichn@gmail.com>
2023-09-26 20:08:58 +0300

    added module scrollreval

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,11 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+ScrollReveal().reveal(".test", {
+   delay: 450,
+   distance: "100%",
+   duration: 600,
+   origin: "left",
+ });
