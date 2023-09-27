412a0d2265343ff827e5d1d46fec955addd3ebf4
maksym-krasovskyi <maxkrasovskyi10@gmail.com>
2023-09-27 17:34:05 +0300

    update menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,7 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+// document.getElementById("select_btn").onclick = function () {
+//   document.getElementById("lang").classList.add("active");
+// };
