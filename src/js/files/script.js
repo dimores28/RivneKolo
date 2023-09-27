185c25ed955480525752eb52d0f511dbb5ca6b20
Dmytro <dmytroivanovichn@gmail.com>
2023-09-27 17:52:37 +0300

    27092023

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,6 +3,10 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
+// document.getElementById("select_btn").onclick = function () {
+//   document.getElementById("lang").classList.add("active");
+// };
+
 
 
 //GSAP====================================================================================================
