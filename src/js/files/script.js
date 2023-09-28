6a4824ffe05713df6ea0ce09c678e9c1b7782ba6
Dmytro <dmytroivanovichn@gmail.com>
2023-09-28 14:58:32 +0300

    test menu animation

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -18,3 +18,24 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.fromTo(".heading__span-1", {opacity: 0, x: -300},{opacity: 1, x: 0, duration: 0.6});
 gsap.fromTo(".heading__span-2", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 0.6});
 
+
+const button = document.querySelector(".menu__icon");
+let active = false;
+
+const openTl = gsap.timeline({ paused: true });
+const closeTl = gsap.timeline({ paused: true });
+
+openTl.to('.menu', {width: '100%', backgroundColor: "white", duration: 0.6})
+      .to('.menu__link', {opacity: 1, duration: 0.6});
+
+
+closeTl.to('.menu__link', {opacity: 0, duration: 1})
+       .to('.menu', {width: '52px', backgroundColor: "transparent", duration: 1})
+
+
+button.addEventListener("click", () => {
+   if (!active) openTl.seek(0).play();
+   else closeTl.seek(0).play();
+
+   active = !active;
+})
