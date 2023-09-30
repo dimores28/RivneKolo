5f276bdfdab4f73a5a1878dfd73f5e3c2c371647
Dmytro <dmytroivanovichn@gmail.com>
2023-09-30 11:28:03 +0300

    30092023

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -31,7 +31,66 @@ const madeinTL = gsap.timeline({
 
  madeinTL.from(".madein__sticker", { transformOrigin: "right center", rotationY: 90,  stagger: 1});
 
+ const madeinTlHero = gsap.timeline({
+  scrollTrigger: {
+    trigger: "hero__wraper",
+    start: "center center",
+    end: "+=100%",
+  },
+});
+
+madeinTlHero.from(".hero__heading", { opacity: 0, x: -400, });
+madeinTlHero.from(".hero__text", { opacity: 0, x: 400, });
+
  const hendTL = gsap.timeline({repeat: -1, yoyo: true});
 //  hendTL.yoyo( true ); 
  hendTL.to('.social__hand', {x: "115%", y: "-19%", duration: 1})
        .to(".social__shadow", {x: "180%", y: "235%", scale: 0.4, duration: 1}, "-=1");
+
+//Animate menu ===========================================
+
+const button = document.querySelector(".menu__icon");
+let active = false;
+
+const openTl = gsap.timeline({ paused: true });
+const closeTl = gsap.timeline({ paused: true });
+
+openTl
+  .to(".menu", { width: "90%", backgroundColor: "white", duration: 0.6 })
+  .to(".menu__link", { opacity: 1, duration: 0.4, delay: 0.3 });
+
+closeTl.to(".menu__link", { opacity: 0, duration: 0.4 }).to(".menu", {
+  width: "51px",
+  backgroundColor: "transparent",
+  duration: 0.6,
+  delay: 0.4,
+});
+
+button.addEventListener("click", () => {
+  if (!active) openTl.seek(0).play();
+  else closeTl.seek(0).play();
+
+  active = !active;
+});
+
+//Animate language ===========================================
+const openButton = document.querySelector(".select_btn");
+let activeMenu = false;
+
+const openMenu = gsap.timeline({ paused: true });
+const closeMenu = gsap.timeline({ paused: true });
+
+openMenu.to(".lang", { height: "210px", duration: 0.6 });
+
+closeMenu.to(".lang", {
+  height: "45px",
+  duration: 0.6,
+  delay: 0.4,
+});
+
+openButton.addEventListener("click", () => {
+  if (!activeMenu) openMenu.seek(0).play();
+  else closeMenu.seek(0).play();
+
+  activeMenu = !activeMenu;
+});
