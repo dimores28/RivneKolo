d25edf3ade390d4150fb153709535eb08cb40326
Dmytro <dmytroivanovichn@gmail.com>
2023-09-30 22:21:09 +0300

    animation step 1

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -15,8 +15,16 @@ import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.fromTo(".heading__span-1", {opacity: 0, x: -300},{opacity: 1, x: 0, duration: 0.6});
-gsap.fromTo(".heading__span-2", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 0.6});
+const productTl = gsap.timeline({
+   scrollTrigger: {
+     trigger: ".products__container",
+     start: "top center",
+     end: "+=25%",
+   }
+ });
+
+ productTl.fromTo(".heading__span-1", {opacity: 0, x: -300},{opacity: 1, x: 0, duration: 0.6});
+ productTl.fromTo(".heading__span-2", {opacity: 0, x: 300}, {opacity: 1, x: 0, duration: 0.6});
 
 const madeinTL = gsap.timeline({
    scrollTrigger: {
@@ -33,7 +41,7 @@ const madeinTL = gsap.timeline({
 
  const madeinTlHero = gsap.timeline({
   scrollTrigger: {
-    trigger: "hero__wraper",
+    trigger: ".hero__wraper",
     start: "center center",
     end: "+=100%",
   },
@@ -265,3 +273,138 @@ function initSliders() {
 }
 
 initSliders();
+
+//About animation ======================================================
+//about__scene_1
+const Scene_1 = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".about__scene_1",
+		start: "center center",
+		end: "+=100%",
+	},
+});
+
+Scene_1.from('.about__scene_1 .row_1', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		.from('.about__scene_1 .row_2', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		.from('.about__scene_1 .row_2 img', {y: 200, opacity: 0, duration: 0.4, ease: "back.out(1.7)"})
+		.from('.about__scene_1 .row_3', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"});
+
+
+
+const Scene_2 = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".about__scene_2",
+		start: "top center",
+		end: "+=40%",
+		// markers: true
+	},
+});
+
+Scene_2.from('.about__scene_2 .row_1', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_2', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_3', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_4', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_4 img', {y: 200, opacity: 0, duration: 0.4, ease: "back.out(1.7)"})
+		 .from('.about__scene_2 .row_5', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_6', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_2 .row_6 img', {y: 200, opacity: 0, duration: 0.4, ease: "back.out(1.7)"});
+
+
+
+const Scene_3 = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".about__scene_3",
+		start: "top center",
+		end: "+=30%",
+		// markers: true
+	},
+});
+
+document.querySelectorAll(".about__scene_3 p").forEach((row) => {  
+	Scene_3.from(row, {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+});
+
+
+const Scene_4 = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".about__scene_4",
+		start: "top center",
+		end: "+=50%",
+		// markers: true
+	},
+});
+Scene_4.from('.about__scene_4 .row_1', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_4 .row_1 img', {y: 200, opacity: 0, duration: 0.4, ease: "back.out(1.7)"})
+		 .from('.about__scene_4 .row_2', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_4 .row_3', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_4 .row_4', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+		 .from('.about__scene_4 .row_5', {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"});
+
+
+const Scene_5 = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".about__scene_5",
+		start: "top center",
+		end: "+=60%",
+		// markers: true
+	},
+});
+
+Scene_5.from('.about__scene_5 .row_1', {y: 200, opacity: 0, duration: 0.4, ease: "back.out(1.7)"});
+document.querySelectorAll(".advanteges__item").forEach((item) => {  
+	Scene_5.from(item, {y: 200, opacity: 0, duration: 0.4, ease: "power3.out"})
+});
+
+
+if(document.querySelector('.about__bagel')) {
+	let mm = gsap.matchMedia(),
+	breakPoint = 768;
+ 
+	mm.add({
+ 
+	  isDesktop: `(min-width: ${breakPoint}px)`,
+	  isMobile: `(max-width: ${breakPoint - 1}px)`,
+	  reduceMotion: "(prefers-reduced-motion: reduce)"
+ 
+	  }, (context) => {
+ 
+	  let { isDesktop, isMobile } = context.conditions;
+	  const contentHeight = document.querySelector('.about__content').offsetHeight;
+ 
+	  if(isDesktop) {
+			
+			const bagel =  gsap.timeline({
+				scrollTrigger: {
+					trigger: ".about__bagel",
+					start: "top 20%",
+					end: `+=${contentHeight * 0.7}`,
+					scrub: 1,
+					pin: true,
+					// markers: true
+				},
+			});
+			
+			bagel.to('.about__bagel img', {rotation: 360, duration: 3});
+	  }
+ 
+	  if(isMobile) {
+			const bagel =  gsap.timeline({
+				scrollTrigger: {
+					trigger: ".about__bagel",
+					start: "top 10%",
+					end: `+=${contentHeight}`,
+					scrub: 1,
+					pin: false,
+					markers: true
+				},
+			});
+			
+			bagel.to('.about__bagel img', {rotation: 360, duration: 3});
+	  }
+ 
+	}); 
+ 
+
+ }
+
+//end about animation ===================================================
